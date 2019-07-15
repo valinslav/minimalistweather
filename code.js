ae3dad373cde647de819729cf6e00fa67f9bf68b
valinslav <52934798+valinslav@users.noreply.github.com>
2019-07-15 16:18:02 -0400

    Update code.js

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ if(tempType == 'imperial') {
     templetter = 'F';
 }
 
-$.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c08f28491a3a2bbb6672e149c1e6342e&units=${tempType}`).done(callBack).fail(jsonFail);
+$.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c08f28491a3a2bbb6672e149c1e6342e&units=${tempType}`).done(callBack).fail(jsonFail);
 
 let pictures = [
 {
